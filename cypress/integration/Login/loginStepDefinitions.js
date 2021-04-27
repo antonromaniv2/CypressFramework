027adb9c063b837193bb77c982d4730a8839b5f3
antonromaniv2 <anton96romaniv@gmail.com>
2021-04-27 16:27:21 +0300

    2nd commit

--- cypress/integration/Login/loginStepDefinitions.js
+++ cypress/integration/Login/loginStepDefinitions.js
@@ -1,15 +1,25 @@
 /// <reference types="cypress" />
 
-import {LoginPage} from "../pages/login_page"
+import {LoginPage} from "c:/My Files/Cypress/CypressFramework/cypress/pages/login_page" 
 
 const loginPage = new LoginPage()
 
-it('loginTest', function(){
-
+Given ('User is on the Login page', function (){
     cy.visit('https://vishnu-qa.linksquares.dev/users/sign_in')
+});
+
+When ('User enters valid credentials', function (){
     loginPage.enterEmail('administrator@example.com')
     loginPage.enterPassword('password1234')
-    loginPage.clickLogin()
-    cy.get('h1').should('contain','Welcome to Vishnu')
+});
 
-})
+And ('User clicks Login', function (){
+    loginPage.clickLogin()
+});
+
+Then ('User is navigated to Projects tab', function (){
+    cy.get('h1').should('contain','Welcome to Vishnu')
+});
+
+
+
